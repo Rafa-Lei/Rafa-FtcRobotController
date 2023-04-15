c7f1da0c3b9a4c656afe91562093092d25599457
Dysprosium-164 <charlie@ploughshare.com>
2023-04-15 15:50:31 -0400

    Created NewTestLine class

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/PrintTestLine.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/PrintTestLine.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.robotcontroller;
 
 public class PrintTestLine {
     public static PrintTestLine; {
+        System.out.println("Waking up...");
         System.out.println("Hello, world!");
     }
 }
